917bc5f4ad204e1e6bc868d24eb90e1d2332b69a
samueljason0508 <145931219+samueljason0508@users.noreply.github.com>
2025-10-12 08:33:08 -0400

    dd

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,14 +2,23 @@ export default function Navbar({ data }) {
   return (
     <div style={{ width: '20%', padding: '10px', overflowY: 'auto' }}>
       <h3>Navbar</h3>
-      <hr/>
+      <hr />
       {data && (
-        <div style={{ marginTop: '20px' }}>
-          <h4>Map Data:</h4>
-          <pre style={{ fontSize: '12px', whiteSpace: 'pre-wrap', wordWrap: 'break-word' }}>
-            {JSON.stringify(data, null, 2)}
-          </pre>
-        </div>
+        <>
+          <div style={{ marginTop: '20px' }}>
+            <h4>Map Data:</h4>
+            <pre style={{ fontSize: '12px', whiteSpace: 'pre-wrap', wordWrap: 'break-word' }}>
+              {JSON.stringify(data, null, 2)}
+            </pre>
+          </div>
+
+          <div style={{ marginTop: '20px' }}>
+            <h4>Gemini Opinion:</h4>
+            <pre style={{ fontSize: '12px', whiteSpace: 'pre-wrap', wordWrap: 'break-word' }}>
+              STUFF
+            </pre>
+          </div>
+        </>
       )}
     </div>
   );
