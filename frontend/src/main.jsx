a76a0d2c15bb0c8b33459230f8a7abcc1b4148e6
samueljason0508 <145931219+samueljason0508@users.noreply.github.com>
2025-10-11 21:53:44 -0400

    re generated front end with vite

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,18 +1,10 @@
-import React from "react";
-import "./Main.css";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
 
-
-function Main() {
-  return (
-    <div className="app">
-      <h1 className="main-title">Sadness</h1>
-      <button className="main-button">Feel the Pain</button>
-    </div>
-  );
-}
-
-export default Main;
-
-
-
-
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
