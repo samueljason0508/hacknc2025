5b38bf820ab86e5edaa7603db50a351bcf6c8688
samueljason0508 <145931219+samueljason0508@users.noreply.github.com>
2025-10-12 10:29:31 -0400

    dd

--- src/components/VoronoiMap.jsx
+++ src/components/VoronoiMap.jsx
@@ -7,7 +7,7 @@ import 'leaflet/dist/leaflet.css';
 import { useUserWeights } from '../services/userWeights';
 import { computeFrustration, colorForSigned } from '../utils/frustrationIndex';
 
-// red marker
+// red marker 
 const redIcon = new L.Icon({
   iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png',
   shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
