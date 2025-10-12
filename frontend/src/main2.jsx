db21c474e86569defeee1b79aad9a9d1f7324ce3
samueljason0508 <145931219+samueljason0508@users.noreply.github.com>
2025-10-11 23:44:53 -0400

    added import react

--- frontend/src/main2.jsx
+++ frontend/src/main2.jsx
@@ -1,5 +1,6 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from "react"
+import { StrictMode } from "react"
+import { createRoot } from "react-dom/client"
 import './index.css'
 import App2 from './App2.jsx'
 
