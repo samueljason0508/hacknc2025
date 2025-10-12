6d5114a7b253a955af84fe5fb0d1d437856fc1be
samueljason0508 <145931219+samueljason0508@users.noreply.github.com>
2025-10-12 02:29:34 -0400

    dd

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  appType: 'mpa', // Multi-page app
-  build: {
-    rollupOptions: {
-      input: {
-        main: './index.html',
-        map: './map.html'
-      }
-    }
-  }
+  plugins: [react()]
 })
