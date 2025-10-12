2107f3092e8c194e21a28a315f1ecec14325ca50
samueljason0508 <145931219+samueljason0508@users.noreply.github.com>
2025-10-12 00:39:24 -0400

    added sadness

--- src/App.vue
+++ src/App.vue
@@ -3,15 +3,10 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+       <div className="app">
+        <h1 className="main-title">Sadness</h1>
+        <button className="main-button">Feel the Pain</button>
+      </div>
 </template>
 
 <style scoped>
